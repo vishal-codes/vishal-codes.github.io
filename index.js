f0da726076230041d54d48a4b77411ab1d92517a
Vishal <itsvishal2417@gmail.com>
2021-07-29 18:35:14 +0530

    Improved Indentation

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+//jshint esversion:8
 // GO TO TOP BUTTON
 //Get the button:
 mybutton = document.getElementById("myBtn");
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
+window.onscroll = function() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
-}
+};
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
