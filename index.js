9e029960ac4af0bee6dad8dd48164fc96ae0f3a4
vishal-codes <itsvishal2417@gmail.com>
2021-12-11 16:57:43 +0530

    Added new project

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 mybutton = document.getElementById("myBtn");
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
